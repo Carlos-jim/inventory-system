7aefe5731ccf5569b006136fe06f3602662c6ef7
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-08-27 13:05:05 -0400

    AAAA

--- app/components/auth/inputRegister.component.jsx
+++ app/components/auth/inputRegister.component.jsx
@@ -123,7 +123,7 @@ const InputsRegisterComponent = () => {
       >
         <button
           type="submit"
-          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-default-color-btn hover:bg-hover-color-btn focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-default-color-btn hover:bg-hover-color-btn focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-outline-color"
         >
           Registrarse
         </button>
