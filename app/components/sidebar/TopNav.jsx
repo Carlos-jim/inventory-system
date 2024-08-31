051d34c71f31998aa334ff2c4c773da0946a0552
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-08-31 15:22:05 -0400

    Update topnav

--- app/components/sidebar/TopNav.jsx
+++ app/components/sidebar/TopNav.jsx
@@ -1,7 +1,9 @@
+import { FaHome } from 'react-icons/fa';
+
 export default function TopNav() {
     return (
       <div className="flex items-center justify-between p-4">
-        <button className="text-white bg-teal-500 rounded-full px-4 py-2">HOME</button>
+        <button className="text-white items-center flex bg-teal-500 rounded-md px-4 py-2"> <FaHome className="mr-2 w-6 h-5" />HOME</button>
       </div>
     );
   }
